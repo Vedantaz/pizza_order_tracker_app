a9d4a49a5f905240380312b450f5e9872b9822b0
Vedantaz <muleyvedant4@gmail.com>
2022-03-15 11:38:52 +0530

    Developement production started , installed all requireed packages else...., error about yarn watch, debug it

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express() // functionality which express provides are stored in app variable.
 const PORT = process.env.PORT || 3300;
-app.listen(PORT, () => {
-    console.log(PORT);
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path')  // it is an inbuilt module in node.js...
 
-    console.log(`Listening on port : ${PORT}`);
-})
+let mix = require('laravel-mix');
+mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss', 'public/css/app.css');
 app.get('/', (req, res) => {
-    res.send('Hello World');
-
+    res.render('home')
+})
+// setting template engine
+app.use(expressLayout);
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname + '/resources/views'));
+app.set('view engine', 'ejs');
+app.listen(PORT, () => {
+    console.log(`Listening on port : ${PORT}`);
 })
